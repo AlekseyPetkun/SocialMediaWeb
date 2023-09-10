cfb05377d486c3875de65331e0c65e1977456980
Aleksey Petkun <aleksey_2710@mail.ru>
2023-09-10 22:32:30 +0300

    creating services for Posts

---
+++ src/main/java/com/github/alekseypetkun/socialmediaweb/service/PostService.java
@@ -0,0 +1,105 @@
+package com.github.alekseypetkun.socialmediaweb.service;
+
+import com.github.alekseypetkun.socialmediaweb.dto.*;
+import com.github.alekseypetkun.socialmediaweb.entity.Post;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Сервис по работе с постами
+ */
+public interface PostService {
+
+    /**
+     * Создание поста по параметрам
+     *
+     * @param dto    параметры поста
+     * @param image  картинка поста
+     * @param userId идентификатор автора поста
+     * @return созданный пост
+     */
+    PostDto addPost(CreatePost dto, MultipartFile image, Long userId);
+
+    /**
+     * Изменение поста по id
+     *
+     * @param postId идентификатор поста
+     * @param dto    параметры поста
+     * @param userId идентификатор автора
+     * @return измененный пост
+     */
+    PostDto updatePostById(Long postId, UpdatePost dto, Long userId);
+
+    /**
+     * Удаление поста по id
+     *
+     * @param postId идентификатор поста
+     * @param userId идентификатор автора
+     */
+    void deletePostById(Long postId, Long userId);
+
+    /**
+     * Показать весь список постов друзей и подписок
+     *
+     * @param pageNumber параметр начальной страницы
+     * @param pageSize   параметр конечной страницы
+     * @param userId     идентификатор авторизованного пользователя
+     * @return список постов друзей и подписок
+     */
+    ResponseWrapperPosts getAllPostsBySubscriptions(int pageNumber, int pageSize, Long userId);
+
+    /**
+     * Получить информацию о посте по id
+     *
+     * @param postId идентификатор поста
+     * @return информация о посте
+     */
+    FullPost getPostById(Long postId);
+
+    /**
+     * Получить посты авторизованного пользователя
+     *
+     * @param pageNumber параметр начальной страницы
+     * @param pageSize   параметр конечной страницы
+     * @param userId     идентификатор авторизованного пользователя
+     * @return список постов автора
+     */
+    ResponseWrapperPosts getPostsMe(int pageNumber, int pageSize, Long userId);
+
+    /**
+     * Изменить картинку поста по ее идентификатору
+     *
+     * @param postId идентификатор поста
+     * @param image  новая картинка поста
+     * @param userId идентификатор авторизованного пользователя
+     * @return true or false
+     */
+    boolean updatePostImage(Long postId, MultipartFile image, Long userId);
+
+    /**
+     * Поиск постов по названию (заголовку)
+     *
+     * @param pageNumber параметр начальной страницы
+     * @param pageSize   параметр конечной страницы
+     * @param title      название поста (заголовок)
+     * @return список постов
+     */
+    ResponseWrapperPosts findByTitlePost(int pageNumber, int pageSize, String title);
+
+    /**
+     * Поиск постов по содержанию
+     *
+     * @param pageNumber параметр начальной страницы
+     * @param pageSize   параметр конечной страницы
+     * @param content    содержание поста
+     * @return список постов
+     */
+    ResponseWrapperPosts findByContentPost(int pageNumber, int pageSize, String content);
+
+    /**
+     * Поиск поста по его идентификатору
+     *
+     * @param postId идентификатор пота
+     * @return найденный пост
+     */
+    Post findPostById(Long postId);
+}
