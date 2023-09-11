db01af24e3a23af5dbb0085643b7f8b701f95e15
Aleksey Petkun <aleksey_2710@mail.ru>
2023-09-11 20:36:24 +0300

    creating repositories

---
+++ src/main/java/com/github/alekseypetkun/socialmediaweb/repository/SubscriberRepository.java
@@ -0,0 +1,23 @@
+package com.github.alekseypetkun.socialmediaweb.repository;
+
+import com.github.alekseypetkun.socialmediaweb.entity.Subscriber;
+import com.github.alekseypetkun.socialmediaweb.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Репозиторий подписчиков
+ */
+@Repository
+public interface SubscriberRepository extends JpaRepository<Subscriber, Long> {
+
+    /**
+     * Получить список подписок пользователя
+     *
+     * @param fromUserId пользователь
+     * @return список подписок
+     */
+    List<Subscriber> findAllByFromUserId(User fromUserId);
+}
--- src/main/java/com/github/alekseypetkun/socialmediaweb/repository/UserRepository.java
+++ src/main/java/com/github/alekseypetkun/socialmediaweb/repository/UserRepository.java
@@ -4,8 +4,17 @@ import com.github.alekseypetkun.socialmediaweb.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Репозиторий пользователей
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    /**
+     * Поиск пользователя по его логину
+     *
+     * @param username логин пользователя
+     * @return пользователь
+     */
     User findByUsername(String username);
 }
